9d0f5891a222b664f4915ee898b661bf9a4086f5
unknown <beto.911599@gmail.com>
2019-11-18 20:02:33 -0600

    json

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/alberto-blog-post";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/petsDB";
 exports.PORT = process.env.PORT || 8080;
